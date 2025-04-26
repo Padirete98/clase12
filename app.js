679c0004ef9b5b6b686e6b6b94cd186758b53591
tessita <tere.reyna.pa.98@gmail.com>
2025-04-26 10:42:43 -0600

    change fetch and fetch with axios

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("estamos probando con git");
+import axios from "axios";
+const URL = "https://futuramaapi.com/api/v2/characters/1";
+
+/* fetch(URL)
+    .then((response) => {
+        if (!response.ok){
+            throw new Error("error en la peticion");
+        }
+        return response.json();
+    })
+    .then((data) =>{
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+*/
+
+axios.get(URL)
+.then(respuesta => {
+    console.log(respuesta.data);
+}).catch(error =>{
+    console.log(error);
+})
